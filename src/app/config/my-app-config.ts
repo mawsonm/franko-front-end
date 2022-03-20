111eb20dfc92284ba93b2f1e8209ccff77cce188
mawsonm <mawsonm@umich.edu>
2022-03-20 17:07:23 -0500

    removed okta config

--- src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  oidc: {
-    clientId: '0oa3u22hjwnkTQVef5d7',
-    issuer: 'https://dev-65887347.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email']
-  }
-}
