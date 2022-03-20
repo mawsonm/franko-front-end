a1271a1d94f1f3754d522e12fe05a662d12461ea
mawsonm <mawsonm@umich.edu>
2022-03-20 17:08:55 -0500

    removed insta api

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -19,19 +19,6 @@ export class HomeService {
 
   constructor(private http: HttpClient) {}
 
-  getInstagramInfo(): any {
-    var options = new HttpHeaders()
-      .append('x-rapidapi-host', 'instagram85.p.rapidapi.com')
-      .append(
-        'x-rapidapi-key',
-        'ac950bfb9cmsha4e84c1a2ec807fp1fe287jsn58591b9e28f1'
-      );
-    return this.http.get(
-      'https://instagram85.p.rapidapi.com/account/_frankie_diamond/info',
-      { headers: options }
-    );
-  }
-
   getProducts(): Observable<Product[]> {
     return this.http
       .get<GetResponseProducts>(`${this.baseUrl}/products`)
